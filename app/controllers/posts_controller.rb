12fcf5403078d201f18af2d9d5ed43498d103e74
Dan Holmes <dh@danielholmes.com>
2020-11-22 15:08:59 +0000

    Image upload and viewing.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:picture, :caption)
   end
 end
